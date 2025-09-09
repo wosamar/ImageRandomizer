8c71648d2ea6fc3494443312435a9b7fc633a47b
wosamar <woodnnng@gmail.com>
2025-09-09 20:41:44 +0800

    refactor: 調整host、port

--- app/app.py
+++ app/app.py
@@ -47,5 +47,5 @@ def get_image(filename):
 
 
 if __name__ == '__main__':
-    # 在 127.0.0.1:5000 上執行應用程式
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
